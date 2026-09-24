9ff809281ec533a5cfac1c4e534991e773a2cd5a
TFLRT <mikeviawashington@mlwrealtygroup.com>
2026-09-23 19:51:19 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,3 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
-'use client';
-
 import React, { useState } from 'react';
 import { 
   Building2, 
