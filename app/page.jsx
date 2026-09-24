aa0210d9c4c42bb75a8f8047f2d69bca83a2611e
TFLRT <mikeviawashington@mlwrealtygroup.com>
2026-09-23 20:00:59 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,6 @@
-'use client';
+import ClientPortal from './ClientPortal';
 
-import React, { useState } from 'react';
-import { 
-  Building2, 
-  UserCheck, 
-  Sparkles, 
-  ShieldCheck, 
-  ArrowRight, 
-  TrendingUp, 
-  Users, 
-  CreditCard,
-  Briefcase,
-  CheckCircle2
-} from 'lucide-react';
+export default function Home() {
+return <ClientPortal />;
+}
+
